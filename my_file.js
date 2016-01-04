9a600c2c60408c6871a0e4dda51e3e1a18e07767
Ben Ebel <bge1234@gmail.com>
2016-01-04 10:24:20 -0700

    initial commit

---
+++ my_file.js
@@ -0,0 +1,5 @@
+var process = require("./processor");
+
+// console.log(process);
+// console.log(process.rate);
+console.log(process.doThat(3));
---
+++ processor.js
@@ -0,0 +1,14 @@
+//Other stuff that doesn't matter.
+
+// module.exports = 42;
+
+// module.exports = {
+//   city: "Denver",
+//   rate: 45.22,
+//   water_level: 68
+// };
+
+module.exports = {
+  doThis: function(num) {return num * 2000},
+  doThat: function(num) {return num * 1000}
+};
